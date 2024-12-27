a5925042f7465ac2fa041ea1724758afa95b4f94
Drathonix <52723768+Drathonix@users.noreply.github.com>
2024-12-27 11:28:03 -0500

    Fabric 1.16.5 Working/Tested

--- src/main/java/com/vicious/loadmychunks/common/mixin/cct/MixinAbstractComputerBlockEntity.java
+++ src/main/java/com/vicious/loadmychunks/common/mixin/cct/MixinAbstractComputerBlockEntity.java
@@ -1,13 +1,21 @@
 package com.vicious.loadmychunks.common.mixin.cct;
-//? if cct {
+
+import com.vicious.loadmychunks.common.LoadMyChunks;
 import com.vicious.loadmychunks.common.mixin.MixinBlockEntity;
-import dan200.computercraft.core.computer.Computer;
+import org.spongepowered.asm.mixin.Mixin;
+
+//? if cct {
 //? if >1.19.2
 import dan200.computercraft.shared.computer.blocks.AbstractComputerBlockEntity;
 //? if <1.19.3
 /*import dan200.computercraft.shared.computer.blocks.TileComputerBase;*/
-import org.spongepowered.asm.mixin.Mixin;
+//?}
 
+//? if !cct {
+/*@Mixin(LoadMyChunks.class)
+public abstract class MixinAbstractComputerBlockEntity {
+}*/
+//?} else {
 //? if >1.19.2
 @Mixin(AbstractComputerBlockEntity.class)
 //? if <1.19.3
